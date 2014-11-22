04abedcac18890971291884dc59e966c815dd207
Michal Rzezniczek <michal.rzezniczek@outlook.com>
2014-11-22 23:31:34 +0100

    add matrix adding function

--- miniAES.py
+++ miniAES.py
@@ -14,3 +14,14 @@ def toMatrix(text, howManyBits):
         output[i].append(letter)
         j += 1
     return output
+
+
+def add(matrix0, matrix1):
+    output = []
+    for i in range(len(matrix0)):
+        k = 0
+        output.append([])
+        for j in matrix0[i]:
+            output[i].append(j + matrix1[i][k])
+            k += 1
+    return output
